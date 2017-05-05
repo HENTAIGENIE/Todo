c7d5bdf7e76f2c5d1d1b86db526d97072ee88832
Jackson Burns <jc3urns@gmail.com>
2017-05-05 16:12:24 -0400

    Initial Commit

---
+++ Task Manager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Task Manager
+//
+//  Created by Jackson Burns on 5/5/17.
+//  Copyright © 2017 Planche Gods. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
